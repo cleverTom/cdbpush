9e3b1d9922398e2e82c44ad34661cccf32cc040d
孟浩然 <wb-mhr264430@alibaba-inc.com>
2017-12-10 11:59:05 +0800

    测试

--- build/index.js
+++ build/index.js
@@ -1,1 +1,11 @@
-kkkk
+console.log(123);
+var a =20;
+var b =30;
+console.log(a);
+console.log(b);
+console.log("hello,world");
+console.log("good");
+console.log("哈哈哈");
+console.log("上传测试!");
+const arr = [1,2,3,4,5];
+console.log(arr);
